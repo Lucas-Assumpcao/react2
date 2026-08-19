6f13475f069cb9f5454bab7d9b8fef7f25fd89bd
Lucas-Assumpcaogit config --global user.email assumpdev@gmail.comgit config --global user.name Lucas-Assumpcao <assumpdev@gmail.com>
2026-08-18 21:25:50 -0300

    react1_footer

--- react2/src/App.jsx
+++ react2/src/App.jsx
@@ -3,19 +3,21 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Sobre from './Sobre';
+import Footer from './componentes/Footer';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      
+
         <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/sobre' element={<Sobre/>}/>
 
         </Routes>
       </BrowserRouter>
-    
+      <Footer/>
+      
     
     
     </>
---
+++ react2/src/componentes/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer (){
+
+    return(
+        <footer className="rodape">
+            <p>2026 todos os direitos reservados</p>
+        </footer>
+    );
+}
+
+export default Footer
