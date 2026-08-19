232af3e17ada16e90c0d8c25c596d2abbcf26989
Lucas-Assumpcaogit config --global user.email assumpdev@gmail.comgit config --global user.name Lucas-Assumpcao <assumpdev@gmail.com>
2026-08-19 20:31:31 -0300

    exerc_completo2

--- react2/src/componentes/Footer.jsx
+++ react2/src/componentes/Footer.jsx
@@ -1,8 +1,10 @@
+import './Footer.css'
 function Footer (){
 
     return(
         <footer className="rodape">
             <p>2026 todos os direitos reservados</p>
+            <img src="instagram.png" alt="logo"  width={50} height={50}/>
         </footer>
     );
 }
